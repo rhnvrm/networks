8fcfaa0f85b61328f9dd8945365be2bcbd85c7d7
Manvendra Singh <manvendra0310@gmail.com>
2016-11-23 23:26:34 +0530

    updated client to integrate multicast, updated structs

--- lab4/client.py
+++ lab4/client.py
@@ -1,40 +1,68 @@
 from jsonsocket import Client
+import multicast 
 
+import threading
 import json
-
-
-class radio_info:
-
-    def __init__(self, packet_type, senders_name):
-        self.type = packet_type
-        self.senders_name = senders_name
+import sys # for exit
 
 host = '0.0.0.0'  # server IP address
 port = 5432 	 # hard coded port
 
 
-def get_radio_info(iradio_req):
+def get_radio_info():
     client = Client()
     client.connect(host, port)
 
+    print "Sending request to server..."
     # object variables as dict
-    client.send(iradio_req.__dict__)
-    response = client.recv()
-    print(response)
+    client.send(iradio_req().__dict__)
+    radio_list = client.recv()
 
-    return response
+    return radio_list
 
+
+def menu_invoke(respnse='NaN', init=0):
+    if init is True:
+        print """
+        Welcome to SNU's best Radio, 
+        Please input options accordingly
+        """
+
+        print """
+        Radio playing at the moment, 
+        Please choose relevant options...
+        P: stop playback and close muticast reception
+        R: resume playback
+        C: change station, view updated radio list 
+        X: terminate the client program
+        """ 
+
+        return str(raw_input())
+
+def play_multicast_radio():
+    # create thread and pass the play multicast radio
+    thread = Thread(target = multicast.client.play_multicast_radio())
+    thread.start()
+
+    rev_input = menu_invoke()
+    if rev_input == 'P':
+        # close the thread
+        # todo: gracefully close the socket and then thread.join()
+    elif rev_input == 'R':
+        # recall same funtion recursively ? search better option
+    elif rev_input == 'C'
+        pass
+    elif rev_input == 'X':
+        # exit program
+        sys.exit() 
+
+    # parent handles te input from the user 
+    # and stops the thread accordingly 
 
 def main():
-    print "Connecting to server.."
     print "Requesting station list from server..."
-    iradio_req = radio_info(10, "mohak")
-    radio_details = get_radio_info(iradio_req)
-"""
-    print_radio_details and get_user_input
-    setup_multicast_server on same program
-    run_multicast_server
-"""
+    radio_list = get_radio_info()
+    menu_invoke(radio_list, 1)
     
 
 if __name__ == '__main__':
--- lab4/iradio_structs.py
+++ lab4/iradio_structs.py
@@ -1,58 +1,64 @@
 # Client to server messages
 class station_info_request:
 
-    def __init__(self, packet_type, senders_name):
-        self.type = packet_type
-        self.senders_name = senders_name
+	def __init__(self, packet_type=10):
+		self.type = 10 # hard_coding packet type to 10 (default)
 
 # Server to client messages
-class site_info:
-
-    def __init__(self, type,
-                 site_name_size, site_name,
-                 site_desc_size, site_desc,
-                 station_count, station_list):
-        self.type = type
-        self.site_name_size = site_name_size
-        self.site_name = site_name # list
-        self.site_desc_size = site_desc_size 
-        self.site_desc
-        self.station_count = station_count
-        self.station_list = station_list
 
 class station_info():
-    def __init__(self, station_number,
-                station_name_size, station_name,
-                multicast_address, data_port, 
-                info_port, bit_rate):
-    self.station_number = station_number
-    self.station_name_size = station_name_size
-    self.station_name = station_name
-    self.multicast_address = multicast_address
-    self.data_port = data_port
-    self.info_port = info_port
-    self.bit_rate = bit_rate
 
+	def __init__(self, station_number,
+				station_name_size, station_name,
+				multicast_address, data_port, 
+				info_port, bit_rate):
+	
+		self.station_number = station_number
+		self.station_name_size = station_name_size
+		self.station_name = station_name
+		self.multicast_address = multicast_address
+		self.data_port = data_port
+		self.info_port = info_port
+		self.bit_rate = bit_rate
+
+
+# station list is a list of all station's station info objects
+class site_info:
+
+	def __init__(self, type,
+				 site_name_size, site_name,
+				 site_desc_size, site_desc,
+				 station_count, station_list):
+		self.type = type
+		self.site_name_size = site_name_size
+		self.site_name = site_name # list
+		self.site_desc_size = site_desc_size 
+		self.site_desc
+		self.station_count = station_count
+		self.station_list = station_list # list of station info type
+
+
+# removed in the latest lab pdf
 
 class station_not_found:
 
-    def __init__(self, type=11, station_number):
-        self.type = 11
-        self.station_number = station_number
+	def __init__(self, station_number, type=11):
+		self.type = 11
+		self.station_number = station_number
 
 
 # Stream information messages (multicast)
 class song_info:
 
-    def __init__(self, type,
-                 song_name_size, 
-                 song_name,
-                 remaining_time_in_sec,
-                 next_song_name_size,
-                 next_song_name):
-        self.type = type
-        self.song_name_size = song_name_size
-        self.song_name = song_name
-        self.remaining_time_in_sec = remaining_time_in_sec
-        self.next_song_name_size = next_song_name_size
-        self.next_song_name = next_song_name
+	def __init__(self, type,
+				 song_name_size, 
+				 song_name,
+				 remaining_time_in_sec,
+				 next_song_name_size,
+				 next_song_name):
+		self.type = type
+		self.song_name_size = song_name_size
+		self.song_name = song_name
+		self.remaining_time_in_sec = remaining_time_in_sec
+		self.next_song_name_size = next_song_name_size # string length 
+		self.next_song_name = next_song_name # string
