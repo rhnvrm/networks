85f567b786a883a57bd4869c98754ac58f80e77f
Manvendra Singh <manvendra0310@gmail.com>
2016-11-25 21:44:27 +0530

    intergrated multicast module

--- lab4/client.py
+++ lab4/client.py
@@ -1,12 +1,13 @@
 from jsonsocket import Client
-import multicast 
+from multicast import iradio_client
+import iradio_structs
 
 import threading
 import json
 import sys # for exit
 
 host = '0.0.0.0'  # server IP address
-port = 5432 	 # hard coded port
+port = 5432       # hard coded port
 
 
 def get_radio_info():
@@ -15,13 +16,17 @@ def get_radio_info():
 
     print "Sending request to server..."
     # object variables as dict
-    client.send(iradio_req().__dict__)
+    client.send(iradio_structs.station_info_request().__dict__)
     radio_list = client.recv()
+    print "radio_list recived"
 
     return radio_list
 
 
-def menu_invoke(respnse='NaN', init=0):
+def print_radio_info(): 
+    pass
+    
+def menu_invoke(response='NaN', init=0):
     if init is True:
         print """
         Welcome to SNU's best Radio, 
@@ -37,32 +42,41 @@ def menu_invoke(respnse='NaN', init=0):
         X: terminate the client program
         """ 
 
-        return str(raw_input())
+    return str(raw_input())
 
 def play_multicast_radio():
-    # create thread and pass the play multicast radio
-    thread = Thread(target = multicast.client.play_multicast_radio())
-    thread.start()
 
-    rev_input = menu_invoke()
-    if rev_input == 'P':
-        # close the thread
-        # todo: gracefully close the socket and then thread.join()
-    elif rev_input == 'R':
-        # recall same funtion recursively ? search better option
-    elif rev_input == 'C'
-        pass
-    elif rev_input == 'X':
-        # exit program
-        sys.exit() 
+    iradio_client.iradio()
 
-    # parent handles te input from the user 
-    # and stops the thread accordingly 
+    # # create thread and pass the play multicast radio
+    # thread = thereading.Thread(target = multicast.client.play_multicast_radio())
+    # thread.start()
+
+    # rev_input = menu_invoke()
+    # if rev_input == 'P':
+    #   # close the thread
+    #   # todo: gracefully close the socket and then thread.join()
+    # elif rev_input == 'R':
+    #   pass
+    #   # recall same funtion recursively ? search better option
+    # elif rev_input == 'C'
+    #   pass
+    # elif rev_input == 'X':
+    #   # exit program
+    #   sys.exit() 
+
+#   # parent handles te input from the user 
+#   # and stops the thread accordingly 
 
 def main():
     print "Requesting station list from server..."
     radio_list = get_radio_info()
-    menu_invoke(radio_list, 1)
+    print "Recieved station list.."
+    print radio_list
+    play_multicast_radio()
+    # print_radio_info(radio_list)
+    
+    # menu_invoke(radio_list, 1)
     
 
 if __name__ == '__main__':
