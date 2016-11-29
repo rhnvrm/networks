a9da1f4eb18bdb6a4cf791bbb2cab24954f7e60c
Rohan Verma <rohanverma2004@gmail.com>
2016-11-29 12:13:07 +0530

    feat: integrate changing radio

--- lab4/static/js/index.js
+++ lab4/static/js/index.js
@@ -7,13 +7,63 @@ $(document).ready(function() {
     $('html').addClass('load-complete');
 });
 
-$('.tool-buttons').click(function(){
+$('#play').click(function(){
 	$.ajax({
 		url: 'http://localhost:5000/toggle',
 		type: 'GET',
 		data: {'submit':true},
 		success: function(result){
-			alert("DONE!");
+			alert(result);
 		}
 	});
 });
+
+$("#r5432").click(function(){
+	$.ajax({
+		url: "http://localhost:5000/change?port=5432",
+		type: "GET",
+		success: function(result){
+			alert(result);
+		}
+	})
+})
+
+$("#r5433").click(function(){
+	$.ajax({
+		url: "http://localhost:5000/change?port=5433",
+		type: "GET",
+		success: function(result){
+			alert(result);
+		}
+	})
+})
+
+$("#r5434").click(function(){
+	$.ajax({
+		url: "http://localhost:5000/change?port=5434",
+		type: "GET",
+		success: function(result){
+			alert(result);
+		}
+	})
+})
+
+$("#r5435").click(function(){
+	$.ajax({
+		url: "http://localhost:5000/change?port=5435",
+		type: "GET",
+		success: function(result){
+			alert(result);
+		}
+	})
+})
+
+$("#r5436").click(function(){
+	$.ajax({
+		url: "http://localhost:5000/change?port=5436",
+		type: "GET",
+		success: function(result){
+			alert(result);
+		}
+	})
+})
